66917bcdc04f0b904bd58d0e66fb75912e513e23
Alain Guillon <zyrass@outlook.fr>
2023-06-08 17:15:48 +0200

    feature: (create) TEST UNITAIRE
    Création des test unitaires avec phpunit

--- tests/ContactControllerTest.php
+++ tests/ContactControllerTest.php
@@ -6,40 +6,157 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ContactControllerTest extends WebTestCase
 {
-
     private $testData;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $jsonData = file_get_contents(__DIR__ . '/DonneesBrut/donneeBrut.json');
+        $jsonData = file_get_contents(__DIR__ . '/JeuxDeDonnees/jeuDonneesPageContact.json');
         $this->testData = json_decode($jsonData, true);
     }
 
-    public function testStatus200()
+    /**
+     * REG-CONTACT-01
+     * Ce test permet de tester que la page /contact existe bien avec un status 200
+     * URL de connexion: /contact
+     * 
+     * @return void OK si requête HTTP retourne le status 200
+     */
+    public function testExistencePageContactOK()
     {
         $client = static::createClient();
         $attendu = $client->request('GET', '/contact');
         $this->assertEquals(
-            $this->testData['status200']['expectedStatusCode'],
+            $this->testData['testExistencePageContactOK']['expectedStatusCode'],
             $client->getResponse()->getStatusCode(),
-            "❌ - Le code d'erreur doit être 200"
+            "❌ - La requête sur /contact doit retourner un status 200"
         );
     }
 
-    public function testTagForm()
+    /**
+     * REG-CONTACT-02
+     * Ce test permet de tester que la page /contacts n'existe pas
+     * URL de connexion: /contacts
+     * 
+     * @return void OK si requête HTTP retourne le status 404
+     */
+    public function testExistencePageContactsKO()
     {
-        $client = WebTestCase::createClient();
-        $attendu = $client->request('GET', '/contact');
-        $this->assertCount(1, $attendu->filter('form'), "❌ - Doit disposer d'un tag form");
+        $client = static::createClient();
+        $attendu = $client->request('GET', '/contacts');
+        $this->assertEquals(
+            $this->testData['testExistencePageContactsKO']['expectedStatusCode'],
+            $client->getResponse()->getStatusCode(),
+            "❌ - La requête sur /contacts doit retourner un status 404"
+        );
     }
 
-    public function testFormInputName()
+    /**
+     * REG-CONTACT-03
+     * Ce test permet de tester l'existence dun tag "form" dans la page /contact
+     * URL de connexion: /contact
+     * 
+     * @return void OK si dans la page /contact il y a bien un tag form
+     */
+    public function testExistenceOneTagFormOK()
     {
         $client = WebTestCase::createClient();
         $attendu = $client->request('GET', '/contact');
-        $this->assertCount(1, $attendu->filter('input[name="contact[name]"]'), "❌ - Doit disposer d'un tag input avec un attribut name avec pour contenu name");
+        $this->assertCount(
+            $this->testData['testExistenceOneTagFormOK']['expectedTagCount'],
+            $attendu->filter('form'),
+            "❌ - La page contact doit impérativement avoir un tag form"
+        );
     }
+
+    /**
+     * REG-CONTACT-04
+     * Ce test permet de tester la non existence dun tag "form" dans la page /contact
+     * URL de connexion: /contact
+     * 
+     * @return void OK si dans la page /contact il n'y a pas de tag form
+     */
+    public function testExistenceOneTagFormKO()
+    {
+        $client = static::createClient();
+        $attendu = $client->request('GET', '/contact');
+        $this->assertCount(
+            $this->testData['testExistenceOneTagFormKO']['expectedTagCount'],
+            $attendu->filter($this->testData['testExistenceOneTagFormKO']['tagSelector']),
+            "❌ - La page contact ne doit pas avoir de tag form"
+        );
+    }
+
+
+    /**
+     * REG-CONTACT-05
+     * Ce test permet de tester l'existence obligatoirement de quatre tags "input" dans la page /contact
+     * URL de connexion: /contact
+     * 
+     * @return void OK si dans la page /contact il y a bien quatre tags input
+     */
+    public function testExistenceDeQuatreInputOK()
+    {
+        $client = static::createClient();
+        $attendu = $client->request('GET', '/contact');
+        $this->assertCount(
+            $this->testData['testExistenceDeQuatreInputOK']['expectedTagCount'],
+            $attendu->filter('input'),
+            "❌ - Doit retourner obligatoirement quatre (4) tags input dans le formulaire"
+        );
+    }
+
+    /**
+     * REG-CONTACT-06
+     * Ce test permet de tester l'existence uniquement de trois tags "input" dans la page /contact
+     * URL de connexion: /contact
+     * 
+     * @return void OK si dans la page /contact il n'y a que trois tags input
+     */
+    public function testExistenceDeQuatreInputKO()
+    {
+        $client = static::createClient();
+        $attendu = $client->request('GET', '/contact');
+        $filteredInputs = $attendu->filter('input')->reduce(function ($node) {
+            return $node->attr('type') !== 'hidden';
+        });
+        $this->assertCount(
+            $this->testData['testExistenceDeQuatreInputKO']['expectedTagCount'],
+            $filteredInputs,
+            "❌ - S'il n'y a que 3 tags input dans le formulaire, il faudra relever une erreur"
+        );
+    }
+
+    /**
+     * REG-CONTACT-07
+     * Ce test permet de tester l'existence de l'attribut name de l'input dont le contenu doit être égal à "nom" dans la page /contact
+     * URL de connexion: /contact
+     * 
+     * @return void OK si dans la page /contact il y a bien un attribut name dont le contenu est "nom"
+     */
+    public function testExistenceDuContenuDansInputNameEgaleNomOK()
+    {
+        $client = static::createClient();
+        $attendu = $client->request('GET', '/contact');
+        $filteredInput = $attendu->filter($this->testData['testExistenceDuContenuDansInputNameEgaleNomOK']['tagSelector'])->first();
+
+        if ($filteredInput->count() > 0) {
+            $this->assertEquals(
+                $this->testData['testExistenceDuContenuDansInputNameEgaleNomOK']['expectedCheckTagContentName'],
+                $filteredInput->attr('name'),
+                "❌ - Doit disposer d'un tag input avec un attribut name avec pour contenu contact[name]"
+            );
+        } else {
+            $this->fail("❌ - L'élément input avec le sélecteur spécifié n'existe pas");
+        }
+    }
+
+
+
+
+
+
+
 
     public function testFormInputEmail()
     {
